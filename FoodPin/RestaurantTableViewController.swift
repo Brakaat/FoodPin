c24ba7862764830925fa955b5c9a9279ed655c42
rabiiwoof <niecheeel@gmail.com>
2020-04-28 18:33:20 +0800

    ver 8 : fix the cell mark

--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -27,14 +27,22 @@ class RestaurantTableViewController: UITableViewController {
         cell.thumbnailImageView.image = UIImage(named: restaurantNames[indexPath.row])
         cell.locationLabel.text = restaurantLocations[indexPath.row]
         cell.typeLabel.text = restaurantTypes[indexPath.row]
-
+        
+        if restaurantIsVisited[indexPath.row] {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        
         return cell
     }
+    
+    var restaurantIsVisited = Array(repeating: false, count: 21)
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .actionSheet)
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        optionMenu.addAction(cancelAction)
+        optionMenu.addAction(cancelAction) // 添加動作至菜單
         
         let callActionHandler = { (action:UIAlertAction!) -> Void in
             let alertMessage = UIAlertController(title: "Unavailable", message: "We will soon to make it update", preferredStyle: .alert)
@@ -48,12 +56,16 @@ class RestaurantTableViewController: UITableViewController {
             (action:UIAlertAction!) -> Void in
             let cell = tableView.cellForRow(at: indexPath)
             cell?.accessoryType = .checkmark
+            self.restaurantIsVisited[indexPath.row] = true
         })
         optionMenu.addAction(checkInAction)
         
-        present(optionMenu, animated: true, completion: nil)
+        present(optionMenu, animated: true, completion: nil) // 呈現菜單
         
-        tableView.deselectRow(at: indexPath, animated: false)
+        tableView.deselectRow(at: indexPath, animated: false) // 取消反選
+        
+        
+        
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
